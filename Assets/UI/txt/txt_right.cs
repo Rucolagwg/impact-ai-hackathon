e7cdb95d6ffd53669f49e0a944e428c63c587e40
jaehyeonhwang <jaehyeon7778@gmail.com>
2025-05-17 21:36:07 +0900

    UI 컴포넌트 완료 및 내부 text 구성

---
+++ Assets/UI/txt/txt_right.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class txt_right : MonoBehaviour
+{
+    public string content;
+    public TMP_Text text;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
